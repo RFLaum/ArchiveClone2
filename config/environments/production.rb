7ac2392999f671d0a672ca02eb99b70e1b590f96
Russell Flaum <zzedar@gmail.com>
2017-12-09 15:26:00 -0500

    more informative error messages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #TODO: remove this before showing it around
+  config.consider_all_requests_local = true
 end
