78637bfc1bfb7873f39e9afd8700812c90a51f04
Russell Flaum <zzedar@gmail.com>
2018-01-04 18:01:36 -0500

    logger 5

--- app/models/concerns/storycount.rb
+++ app/models/concerns/storycount.rb
@@ -33,12 +33,27 @@ module Storycount
     #finds the most common tags used within a given set of stories. story_set
     #is an ActiveRecord::Relation
     def most_common(story_set, num = 10)
-      logger.info "reached most_common"
-      joins(:stories).merge(story_set)
-                     .select("#{table_name}.*, COUNT(*) AS cnt")
-                     .group(pfj)
-                     .reorder('cnt DESC')
-                     .limit(num)
+      i = 0
+      logger.info "most_common #{i += 1}"
+      answer = joins(:stories)
+      logger.info "most_common #{i += 1}"
+      answer = answer.merge(story_set)
+      logger.info "most_common #{i += 1}"
+      answer = answer.select("#{table_name}.*, COUNT(*) AS cnt")
+      logger.info "most_common #{i += 1}"
+      answer = answer.group(pfj)
+      logger.info "most_common #{i += 1}"
+      answer = answer.reorder('cnt DESC')
+      logger.info "most_common #{i += 1}"
+      answer = answer.limit(num)
+      logger.info "most_common #{i += 1}"
+
+      answer
+      # joins(:stories).merge(story_set)
+      #                .select("#{table_name}.*, COUNT(*) AS cnt")
+      #                .group(pfj)
+      #                .reorder('cnt DESC')
+      #                .limit(num)
     end
 
     def cloud_names(num = 9)
